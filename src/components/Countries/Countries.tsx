c462bd68e462927161cdd6da0d670587001730ca
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-24 11:59:52 +0600

    Created Countries, CountryData comps

---
+++ src/components/Countries/Countries.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+  return (
+    <div className='col-4'>
+      Countries
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/components/CountryData/CountryData.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryData = () => {
+  return (
+    <div className='col-8'>
+      CountryData
+    </div>
+  );
+};
+
+export default CountryData;
