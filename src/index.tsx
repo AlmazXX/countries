0d64fc74fedd594fc4266b0aa07ceac87db97b98
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-24 20:41:03 +0600

    Added bootsrap and base styles to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 axios.defaults.baseURL = 'https://restcountries.com/v2';
 
