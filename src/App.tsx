c49db085bf1d8b4d82c8296be292f9f407fe5e11
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-24 20:45:24 +0600

    Added logic to App comp Added layout for rendering comps Added fetchCountries function to get countries list Added useCallback, useEffect to reuse fetchCountries Added countries, selectedAlpha states Added onClickEvent func to set selectedAlpha

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import Countries from "./components/Countries/Countries";
+import CountryData from "./components/CountryData/CountryData";
+import { ApiCountry } from "./types";
 
 function App() {
+  const [countries, setCountries] = useState<ApiCountry[]>([]);
+  const [selectedAlpha, setSelectedAlpha] = useState<null | string>(null);
+
+  const fetchCountries = useCallback(async () => {
+    const countriesResponse = await axios.get<ApiCountry[]>("/all?fields=alpha3Code,name");
+    setCountries(countriesResponse.data);
+  }, []);
+
+  useEffect(() => {
+    fetchCountries().catch((e) => console.error(e));
+  }, [fetchCountries]);
+
+  const onClickEvent = (alpha: string) => {
+    setSelectedAlpha(alpha);
+  };
+
   return (
     <React.Fragment>
-
+      <div className="container py-5">
+        <div className="row justify-content-between">
+          <Countries countries={countries} onClickEvent={onClickEvent} />
+          <CountryData alpha={selectedAlpha} />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
