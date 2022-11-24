b04fe36ea2a4a46d7cbbcbcb763e560ff6ef01c6
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-24 20:38:16 +0600

    Added layout for Countries comp

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
+import { ApiCountry } from "../../types";
+import Country from "./Country";
 
-const Countries = () => {
+interface Props {
+  countries: ApiCountry[];
+  onClickEvent: (id: string) => void;
+}
+
+const Countries: React.FC<Props> = ({ countries, onClickEvent }) => {
   return (
-    <div className='col-4'>
-      Countries
+    <div className="col-4 border border-white p-4 rounded-5">
+      <h3>Countries</h3>
+      <div
+        className="d-flex flex-column overflow-auto mt-3"
+        style={{ height: "80vh" }}
+      >
+        <ul>
+          {countries.map((country) => (
+            <Country
+              key={country.alpha3Code}
+              country={country}
+              onClick={() => onClickEvent(country.alpha3Code)}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
